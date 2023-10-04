4f1b725b5ec9ec00e592827e40fc4fa514255096
ritu1011 <chsverma7@gmail.com>
2023-10-04 13:46:51 +0530

    add lecture notes

--- Lecture-4/App.js
+++ Lecture-4/App.js
@@ -17,6 +17,30 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7OCbzvRLLbTMEOxv1E5fFBQ4N2cfJW-PSg6MeEtLJ&s" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
 
+    )
+}
+const AppLayout = () => {
+    return (
+        <div className="App">
+            <Header />
+        </div>
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(<HeadingComponent2 />)
+root.render(<AppLayout />)
